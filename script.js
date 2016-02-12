6779c6aecb44e1ade385e1de29ab51ed2995f7cb
ferbarra2207 <fernandobarra2207@gmail.com>
2016-02-12 00:26:05 -0500

    Script updated

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+var currentIndex = 0,
+    slides = $('.container .portfolio-slider'),
+    numberOfSlides = slides.length;
+    
+function switchSlides() {
+    var slide = slides.eq(currentIndex);
+}
+
 $(document).ready(function () {
+  // Scroll Feature.
   $("a[href^='#']").on("click", function(e) {
     e.preventDefault();
     var target = this.hash;
     var $target = $(target);
     
     $("html, body").stop().animate({"scrollTop": $target.offset().top}, 1000, "swing");
-  });
+  });  // End of scroll feature
 });
