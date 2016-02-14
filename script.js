bf953f7b5cfce18ece6b7e34dc5b6cd813d90e28
Fernando Barra <fernandobarra2207@gmail.com>
2016-02-14 15:17:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var currentIndex = 0,
-    slides = $('.container .portfolio-slider'),
+    slides = $('#portfolio .portfolio-slider'),
     numberOfSlides = slides.length;
     
 function switchSlides() {
-    var slide = slides.eq(currentIndex);
+    var slide = $('#portfolio .portfolio-slider').eq(currentIndex);
+    slides.hide();
+    slide.css('display', 'inline-block');
 }
 
 $(document).ready(function () {
@@ -15,4 +17,20 @@ $(document).ready(function () {
     
     $("html, body").stop().animate({"scrollTop": $target.offset().top}, 1000, "swing");
   });  // End of scroll feature
+  
+  $('#next').click(function() {
+      currentIndex += 1;
+      if (currentIndex > numberOfSlides - 1) {
+          currentIndex = 0;
+      }
+      switchSlides();
+  });
+  
+  $('#previous').click(function() {
+      currentIndex -= 1;
+      if (currentIndex < 0) {
+          currentIndex = numberOfSlides - 1;
+      }
+      switchSlides();
+  });
 });
