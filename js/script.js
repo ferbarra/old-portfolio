645ea8c9209b410a8287408eb068e171ca7d5809
Fernando Barra <fernandobarra2207@gmail.com>
2016-02-19 12:43:54 -0700

    Form animation created and nav improved

--- js/script.js
+++ js/script.js
@@ -46,4 +46,19 @@ $(document).ready(function () {
           switchSlides();
       }
   });
+  
+  //Floating label headings for the contact form.
+  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+  });
+  
+  // Closes the Menu on small devices when clicked.
+  $(".navbar-collapse ul li a:not(.dropdown-toggle)").click(function() {
+      $(".navbar-toggle:visible").click();
+  })
+  
 });
